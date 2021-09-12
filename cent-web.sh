653a8906a9d691446e140079baee100ecd8ff618
xconer <90569268+xconer@users.noreply.github.com>
2021-09-13 00:09:39 +0700

    Create cent-web.sh

---
+++ cent-web.sh
@@ -0,0 +1,38 @@
+sudo yum install epel-release -y
+sudo yum install nginx -y
+sudo systemctl start nginx
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+sudo systemctl enable nginx
+sudo setsebool -P httpd_can_network_connect 1
+
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+sudo yum clean all && sudo yum makecache fast
+sudo yum install -y gcc-c++ make
+sudo yum install -y nodejs
+npm i pm2 bytenode -g
+
+sudo yum install epel-release -y
+sudo yum install snapd -y
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install core
+sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+sleep 3
+
+wget https://raw.githubusercontent.com/xconer/wallet/main/nginx.conf -O /etc/nginx/nginx.conf
+sleep 2
+systemctl restart nginx
+
+sleep 1
+cd /home
+wget https://raw.githubusercontent.com/xconer/wallet/main/web.zip -O /home/web.zip
+sleep 1
+unzip web.zip
+sleep 1
+cd /home/web
+npm i
