cc20a32de9b6c0ca51efda6475e8fdc3f298cf39
xconer <90569268+xconer@users.noreply.github.com>
2021-09-12 23:39:38 +0700

    Create cent-setup.sh

---
+++ cent-setup.sh
@@ -0,0 +1,23 @@
+sudo yum install epel-release -y
+sudo yum install nginx -y
+sudo systemctl start nginx
+sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+sudo systemctl enable nginx
+sudo setsebool -P httpd_can_network_connect 1
+
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+sudo yum clean all && sudo yum makecache fast
+sudo yum install -y gcc-c++ make
+sudo yum install -y nodejs
+npm i pm2 bytenode -g
+
+sudo yum install epel-release -y
+sudo yum install snapd -y
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install core
+sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
