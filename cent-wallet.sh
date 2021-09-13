5c588361dd0f8f3f6b6c0af5a91319092b01c1e7
xconer <90569268+xconer@users.noreply.github.com>
2021-09-13 16:49:00 +0700

    Update cent-wallet.sh

--- cent-wallet.sh
+++ cent-wallet.sh
@@ -1,5 +1,6 @@
 sudo yum install epel-release -y
 sudo yum install nginx -y
+sudo yum install unzip -y
 sudo systemctl start nginx
 sudo firewall-cmd --permanent --zone=public --add-service=http 
 sudo firewall-cmd --permanent --zone=public --add-service=https
